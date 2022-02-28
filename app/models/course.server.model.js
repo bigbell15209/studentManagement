322ef245f1270f753a9219a7124c02536bb25692
Jay <jkim458@my.centennialcollge.ca>
2022-02-28 14:25:25 -0500

    first commit

---
+++ app/models/course.server.model.js
@@ -0,0 +1,34 @@
+﻿const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const CourseSchema = new Schema({
+    created: {
+        type: Date,
+        default: Date.now
+    },
+    courseCode: {
+        type:String,
+        default: '',
+        trim: true,
+        required: 'Code cannot be blank'
+    },
+    courseName: {
+        type: String,
+        default: '',
+        trim: true,
+        required: 'Title cannot be blank'
+    },
+    section: {
+        type: Number, 
+        default: '',
+        trim: true
+    },
+    semester: {
+        type: Number
+    },
+    
+    applicants: [{
+        type: Schema.ObjectId,
+        ref: 'Student'
+    }]
+});
+mongoose.model('Course', CourseSchema);
